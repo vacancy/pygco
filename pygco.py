ccd4ee1f214d020e4e2beb9b1e18097f3649046a
Jiayuan Mao <maojiayuan@gmail.com>
2019-01-10 22:47:02 -0500

    [master] fix import

--- pygco.py
+++ pygco.py
@@ -1,6 +1,6 @@
 import numpy as np
 import ctypes as ct
-from cgco import _cgco
+from .cgco import _cgco
 
 # keep 4 effective digits for the fractional part if using real potentials
 # make sure pairwise * smooth = unary so that the unary potentials and pairwise
