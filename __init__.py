9fe7e87d935406e1c485d8337ec9fe8a577c7601
Jiayuan Mao <maojiayuan@gmail.com>
2020-01-07 17:09:51 -0500

    [master] add auto travis

--- __init__.py
+++ __init__.py
@@ -7,5 +7,14 @@
 #
 # Distributed under terms of the MIT license.
 
-from .pygco import cut_general_graph, cut_grid_graph, cut_grid_graph_simple
+try:
+    from jacinle.jit.cext import auto_travis
+    auto_travis(__file__, required_fies=['libcgco.so'])
+except:
+    pass
+
+try:
+    from .pygco import cut_general_graph, cut_grid_graph, cut_grid_graph_simple
+except ImportError as e:
+    raise ImportError('Auto travis for pygco failed. Run ./travis.sh manually.') from e
 
